0fa939a3c3f52c4592ede88580f4ce3efdec67e9
YoDaMned59 <sduvivier59112@gmail.com>
2024-12-28 12:51:06 +0100

    first commit

---
+++ src/containers/Slider/index.js
@@ -0,0 +1,65 @@
+import { useEffect, useState } from "react";
+import { useData } from "../../contexts/DataContext";
+import { getMonth } from "../../helpers/Date";
+
+import "./style.scss";
+
+const Slider = () => {
+  const { data } = useData();
+  const [index, setIndex] = useState(0);
+  const byDateDesc = data?.focus.sort((evtA, evtB) =>
+    // inversion de l'ordre des dates pour avoir les plus récentes en premier  
+    new Date(evtA.date) < new Date(evtB.date) ? 1 : -1
+  );
+
+  const dataLength = data?.focus.length
+  const nextCard = () => {
+    setTimeout(
+      // Ajout de -1 après "length"
+      () => setIndex(index < dataLength - 1 ? index + 1 : 0),
+      5000
+    );
+  };
+
+  useEffect(() => {
+    nextCard();
+  });
+
+  return (
+    <div className="SlideCardList">
+      {byDateDesc?.map((events, idx) => (
+        <>
+          <div
+            key={events.title}
+            className={`SlideCard SlideCard--${
+              index === idx ? "display" : "hide"
+            }`}
+          >
+            <img src={events.cover} alt="forum" />
+            <div className="SlideCard__descriptionContainer">
+              <div className="SlideCard__description">
+                <h3>{events.title}</h3>
+                <p>{events.description}</p>
+                <div>{getMonth(new Date(events.date))}</div>
+              </div>
+            </div>
+          </div>
+          <div className="SlideCard__paginationContainer">
+            <div className="SlideCard__pagination">
+              {byDateDesc.map((event, radioIdx) => (
+                <input
+                  key={`${event.title}`}
+                  type="radio"
+                  name="radio-button"
+                  checked={index === radioIdx}
+                />
+              ))}
+            </div>
+          </div>
+        </>
+      ))}
+    </div>
+  );
+};
+
+export default Slider;
