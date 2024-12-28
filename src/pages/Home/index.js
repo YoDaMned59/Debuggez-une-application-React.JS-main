20a113cd792ec21bb6e8d6902bbbdc9326d614e3
YoDaMned59 <sduvivier59112@gmail.com>
2024-12-28 13:48:25 +0100

    mise a jour des ancres (Menu et Home)

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,3 +1,4 @@
+
 import Menu from "../../containers/Menu";
 import ServiceCard from "../../components/ServiceCard";
 import EventCard from "../../components/EventCard";
@@ -19,7 +20,7 @@ const Page = () => {
       <Menu />
     </header>
     <main>
-      <section className="SliderContainer">
+      <section id="ServicesContainer" className="SliderContainer">
         <Slider />
       </section>
       <section className="ServicesContainer">
@@ -51,11 +52,11 @@ const Page = () => {
           </ServiceCard>
         </div>
       </section>
-      <section className="EventsContainer">
+      <section id="EventsContainer" className="EventsContainer">
         <h2 className="Title">Nos réalisations</h2>
         <EventList />
       </section>
-      <section className="PeoplesContainer">
+      <section id="PeoplesContainer" className="PeoplesContainer">
         <h2 className="Title">Notre équipe</h2>
         <p>Une équipe d’experts dédiés à l’ogranisation de vos événements</p>
         <div className="ListContainer">
